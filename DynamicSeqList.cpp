c4e531337d2571ba2a89ac7c2e5dd15f6629554e
pandafuture <2633024197@qq.com>
2025-06-16 20:49:52 +0800

    静态顺序表和动态顺序表

---
+++ DynamicSeqList.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstdlib>  // 包含malloc和free函数
+
+// 动态顺序表结构
+struct DynamicSeqList {
+    int *data;  // 指向动态分配数组的指针，指向这一片存储空间的存储地址
+    int maxSize;  // 数组的最大容量
+    int length;  // 当前长度
+};
+
+
+
+// 初始化动态顺序表，size为初始分配的大小
+void InitList (DynamicSeqList &L, int size) {
+    L.data = (int *)malloc(size * sizeof(int));  // 申请size个大小为int的空间，并用malloc函数返回一个强制转换为int类型的指向这片空间开始地址的指针
+    if(!L.data)
+        exit(1);  // 如果分配失败，则退出程序
+    L.length = 0;  // 初始长度为0
+    L.maxSize = size;  // 记录最大容量
+}
+
+
+
+// 增加动态顺序表的容量（增加len个长度）
+void IncreaseSize(DynamicSeqList &L, int len) {
+    int *p = L.data;  // 保存原数组指针
+    // 申请新的更大的空间
+    L.data = (int *)malloc((L.maxSize + len) * sizeof(int));
+    if(!L.data)
+        exit(1);  // 如果分配失败，则退出程序
+    
+    // 将原数组的元素复制到新数组
+    for(int i = 0; i < L.length; i++) {
+        L.data[i] = p[i];
+    }
+    L.maxSize += len;  // 更新最大容量
+    free(p);  // 释放原空间
+}
+
+
+
+// 在顺序表第i个位置插入元素e
+bool ListInsert(DynamicSeqList &L, int i, int e) {
+    // 判断插入位置是否合法，i必须在1~length+1之间
+    if(i < 1 || i > L.length + 1) 
+        return false;
+    // 判断表是否已满
+    if(L.length >= L.maxSize)
+        return false;
+
+    // 将第i个元素及其后面的元素往后移一个位置（从最后一个元素开始）
+    for(int j = L.length; j >= i; j--) {
+        L.data[j] = L.data[j - 1];  // 将前一个位置的元素赋值给当前位置
+    }
+    L.data[i -1] = e;  // 当位置i-1出插入元素e
+    L.length++;  // 表长加1
+    return true;
+}
+
+
+
+// 删除表中第i个元素，并用e返回
+bool ListDelete(DynamicSeqList &L, int i, int &e) {
+    // 判断删除位置是否合法，i必须要在1~length之间
+    if(i < 1 || i > L.length)
+        return false;
+    e = L.data[i -1];  // 保存被删除元素的值
+
+    // 将第i个元素之后的所有元素前移一位（从第一个元素开始）
+    for(int j = i; j < L.length; j++) {
+        L.data[j - 1] = L.data[j];  // 将后一个位置的元素赋值给前一个元素
+    }
+    L.length--;  // 表长减1
+    return true;
+}
+
+
+
+// 按值查找：找到第一个值为e的元素的位序，找不到返回0
+int LocateElem(DynamicSeqList L, int e) {
+    for(int i = 0; i < L.length; i++) {
+        if(L.data[i] == e)
+            return i + 1;  // 返回位序
+    }
+    return 0;  // 未找到
+}
+
+
+
+// 按位查找：返回列表中第i个元素的值
+int GetElem(DynamicSeqList L, int i) {
+    return L.data[i -1];
+}
+
+
+
+// 遍历打印顺序表
+void PrintList(DynamicSeqList L) {
+    for(int i = 0; i < L.length; i++){
+        std::cout << L.data[i] << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ StaticSeqList.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+#define MAX_SIZE 100  // 定义静态顺序表的最大程度
+// 静态顺序表结构体
+struct StaticSeqList {
+    int data[MAX_SIZE];  // 静态数组：用于存储顺序表的元素
+    int length;  // 当前顺序表的长度（即元素个数）
+};
+
+
+
+// 初始化顺序表
+void InitList(StaticSeqList &L){
+    L.length = 0; // 将顺序表的长度初始化为0，表示空表
+}
+
+
+
+// 在顺序表第i个元素插入元素e
+bool ListInsert(StaticSeqList &L, int i, int e) {
+    // 判断插入位置是否合法，i是位序，在1 ~ length+1之间
+    if(i < 1 || i > L.length +1)
+        return false;
+    // 判断表是否已满
+    if(L.length >= MAX_SIZE)
+        return false;
+    
+    // 将第i个元素及其后面的元素都后移一个位置（从最后一个元素开始移动）
+    for(int j = L.length; j >= i; j--) {
+        L.data[j] = L.data[j - 1];  // 把前一个位置的元素赋值给当前位置
+    }
+    L.data[i - 1] = e;  // 在位置为i-1（数组下标）除插入元素e
+    L.length++;  // 表长增加1
+    return true;
+}
+
+
+
+// 删除顺序表第i个位置的元素，并用e返回
+bool ListDelete(StaticSeqList &L, int i, int &e) {
+    // 判断删除位置是否合法， i必须在1~length之间
+    if(i < 1 || i > L.length)
+        return false;
+
+    // 保存被删除元素的值
+    e = L.data[i - 1];
+
+    // 将第i个元素之后的元素向前移一个位置（从第i个位置开始）
+    for(int j = i; j < L.length; j++) {
+        L.data[j - 1] = L.data[j];  // 将后一个位置的元素赋值给前一个位置
+    }
+    L.length--;  // 表长减1
+    return true;
+}
+
+
+
+// 按值查找：返回顺序表中第一个值为e的元素的位序，找不到返回0
+int LacateElem(StaticSeqList L, int e) {
+    for(int i = 0; i < L.length; i++) {
+        if(L.data[i] == e)
+            return i + 1;  // 返回位序
+    }
+    return 0;
+}
+
+
+
+// 按位查找：返回顺序表中第i个元素的值
+int GetElem(StaticSeqList L, int i) {
+    return L.data[i - 1];  // 第i个元素存放在数组中的第i-1个位置
+}
+
+
+
+// 遍历打印顺序表
+void PrintList(StaticSeqList L) {
+    for(int i = 0; i < L.length; i++) {
+        std::cout << L.data[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main(){
+    StaticSeqList L;  // 新建一个静态顺序表L
+    int e;  // 用来返回删除的值
+    InitList(L);  // 初始化静态顺序表L
+
+    ListInsert(L, 1, 1);  // 在L的第一个位序插入1
+    ListInsert(L, 2, 2);  // 在L的第二个位序插入2
+    ListInsert(L, 3, 3);  // 在L的第三个位序插入3
+    ListInsert(L, 4, 4);  
+    ListInsert(L, 5, 5);
+    PrintList(L);  // 遍历打印顺序表
+
+    ListDelete(L, 1, e);  // 删除L中的一号位置的元素，并返回其值
+    PrintList(L);  // 遍历打印顺序表
+
+    std::cout << LacateElem(L, 3);  // 按值查找操作
+    std::cout << std::endl;
+
+    std::cout << GetElem(L, 1) << " ";  // 按位查找第1个位序的元素
+    std::cout << std::endl;
+
+    PrintList(L);  // 遍历打印顺序表
+    return 0;
+}
