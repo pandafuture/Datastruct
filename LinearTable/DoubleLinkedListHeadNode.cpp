3412c4bd1b67be8c9b79b5b91cabeed1a4936f31
pandafuture <2633024197@qq.com>
2025-07-19 12:41:36 +0800

    双链表（带头结点）

--- LinearTable/DoubleLinkedListHeadNode.cpp
+++ LinearTable/DoubleLinkedListHeadNode.cpp
@@ -16,6 +16,12 @@ void InitList(DNode *&L) {
 }
 
 
+// 判空
+bool Empty(DNode *L) {
+    return L -> next == nullptr;  // 返回头结点的 next 是否为空
+}
+
+
 // 在第 i 个位置插入元素 e
 bool ListInsert(DNode *L, int i, int e) {
     if(i < 1)
@@ -68,8 +74,56 @@ bool ListDelete(DNode *L, int i, int &e) {
 }
 
 
-// 遍历打印链表
-void PrintList(DNode *L) {
+// 按值查找
+int LocateElem(DNode *L, int e) {
+    DNode *p = L -> next;  // 新建一个指针 p ，指向第一个结点
+    int j = 1;  // 设置一个计数器，记录当前指向第几个结点，默认为 1 ，指向第一个结点
+    while(p) {
+        if(p -> data == e) 
+            return j;
+        p = p -> next;
+        j++;
+    }
+    return 0;  // 未找到则返回 0
+}
+
+
+// 按位查找
+int GetElem(DNode *L, int i) {
+    // 判断输入是否合法，不能小于 1
+    if(i < 1) 
+        return -1;
+    
+    DNode *p = L -> next;  // 新建一个指针，指向第一个结点
+    int j = 1;  // 设置一个计数器，记录当前指向第几个结点
+    // 遍历整个链表，找到第 i 个结点
+    while(p && j < i) {
+        p = p -> next;
+        j++;
+    }
+    if(!p)  // 若没找到则返回 -1
+        return -1;
+    // 若找到，则返回其值
+    return p -> data;
+}
+
+
+// 求表长
+int Length(DNode *L) {
+    int length = 0;  // 设置一个表长计数器，记录表长
+    DNode *p = L -> next;  // 新建一个指针 p ，指向第一个结点
+    //遍历整个链表，每遍历一个结点就记录一个
+    while(p) {
+        length++;
+        p = p -> next;
+    }
+    // 返回表长
+    return length;
+}
+
+
+// 后向遍历输出链表
+void PrintList_BackwardTraversal(DNode *L) {
     DNode *p = L -> next;  // 新建一个指针 p ，指向第一个结点
     while(p) {
         std::cout << p -> data << " ";  // 遍历输出当前结点的值
@@ -79,21 +133,44 @@ void PrintList(DNode *L) {
 }
 
 
+// 销毁操作
+void DestroyList(DNode *&L) {
+    DNode *p = L -> next;  // 新建一个指针 P ，指向第一个结点
+    while(p) {
+        DNode *temp = p;  // 新建一个指针 temp ，指向 p 指向的结点
+        p = p -> next;
+        delete temp;  // 删除结点
+    }
+    delete L;  // 删除头结点
+    L = nullptr;  // 头指针指向空
+}
+
+
 int main() {
     DNode *L;  // 新建一个双链表 L
     int e;
     InitList(L);  // 初始化 L
+    std::cout << "判空：" << Empty(L) << std::endl;  // 判空操作
+    std::cout << "表长：" << Length(L) << std::endl;  // 求表长
 
     ListInsert(L, 1, 1);  // 在第一个位置插入 1
     ListInsert(L, 2, 2);
     ListInsert(L, 3, 3);
-    PrintList(L);  // 遍历打印链表
+    std::cout << "输出: ";
+    PrintList_BackwardTraversal(L);  // 遍历打印链表
 
-    ListInsert(L, 2, 4);  // 在第一个位置插入 4
-    PrintList(L);
+    ListInsert(L, 2, 4);  // 在第二个位置插入 4
+    std::cout << "输出：";
+    PrintList_BackwardTraversal(L);
 
-    ListDelete(L, 2, e);  // 删除第 2 个位置的元素并用 e 返回
-    PrintList(L);
-    std::cout << e << " ";
+    ListDelete(L, 2, e);  // 删除第 2 个位置的元素并用 e 返回 
+    std::cout << "输出：";
+    PrintList_BackwardTraversal(L);
+    std::cout << "删除元素为：" << e << std::endl;
+
+    std::cout << "判空：" << Empty(L) << std::endl;  // 判空操作
+    std::cout << "表长：" << Length(L) << std::endl;  // 求表长
+
+    DestroyList(L);  // 销毁操作
     return 0;
 }
